13c5da46e43cd68497aa4ba5f39cf8134dda41e0
yanmiao <worldneedevolution@gmail.com>
2021-01-05 21:26:46 +0800

    yes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var app = new Vue({
             var req = 'http://192.168.1.4:8000/production/?method=common_query&params={%22tb%22:%20%22hub_kind%22,%22condition%22:[{%22name%22:%22name%22,%22condition%22:%22=%22,%22value%22:%22%E6%8B%89%E9%93%BE%22}],%22columns%22:[%22id%22,%22name%22]}';
             axios.get(req)
                 .then(function (response) {
-                    console.lo  g(response);
+                    console.log(response);
                 })
                 .catch(function (error) {
                     console.log(error);
